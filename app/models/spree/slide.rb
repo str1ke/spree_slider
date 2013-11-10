fb7ba07be64d0ddbe7b266d023ba904e0ee2a07a
Andrey Sokolov <a.sokolov@speople.pro>
2013-11-10 20:31:53 +0700

    fix for spree2 and rails4

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,17 +1,16 @@
 class Spree::Slide < ActiveRecord::Base
 
   has_attached_file :image
+
   include Spree::Core::S3Support
   supports_s3 :image
 
-  scope :published, where(:published => true)
-
-  #attr_accessible :name, :body, :link_url, :published, :image, :position, :product_id
+  scope :published, -> { where(:published => true) }
 
   belongs_to :product
 
   def initialize(attrs = nil)
-    attrs ||= {:published => true}
+    attrs ||= { :published => true }
     super
   end
 
